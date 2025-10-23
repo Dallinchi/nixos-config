c2d65c8758bad9f89ea26c796b545ee2f2ee150f
Dallinchi <herooopr@gmail.com>
2025-10-23 22:40:26 +0300

    services: greetd niri to default

--- core/services.nix
+++ core/services.nix
@@ -90,7 +90,7 @@
       settings = {
         default_session = {
           user = username;
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland"; # start Hyprland with a TUI login manager
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd niri"; # start Hyprland with a TUI login manager
         };
       };
     };
