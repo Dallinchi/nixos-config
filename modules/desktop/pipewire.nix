e80bbedf3f9dd8609e59092df4a68962fded7708
Dallinchi <herooopr@gmail.com>
2026-08-11 13:30:28 +0300

    pipewire: improve settings

--- modules/desktop/pipewire.nix
+++ modules/desktop/pipewire.nix
@@ -8,21 +8,19 @@
       wireplumber.enable = true;
 
       extraConfig.pipewire = {
-        "92-sample-rate" = {
+        "92-audio-properties" = {
           "context.properties" = {
+            # Базовая частота дискретизации
             "default.clock.rate" = 48000;
-            "default.clock.allowed-rates" = [ 48000 ];
+            # Разрешаем переключение на 44.1kHz без ресэмплинга, если трек этого требует
+            "default.clock.allowed-rates" = [ 44100 48000 ];
+            
+            # Настройки буфера (квантования)
+            "default.clock.quantum" = 1024;      # Размер буфера по умолчанию
+            "default.clock.min-quantum" = 32;     # Позволяет снижать задержку, если нужно приложению
+            "default.clock.max-quantum" = 2048;   # Порог для предотвращения заиканий при высокой нагрузке
           };
         };
-
-        "92-buffer" = {
-          "context.properties" = {
-            "default.clock.min-quantum" = 1024;
-            "default.clock.quantum" = 1024;
-            "default.clock.max-quantum" = 2048;
-          };
-        };
-
       };
     };
   };
