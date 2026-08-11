b230cba6dc115ed7eeec0c12ce57e2c963516deb
Dallinchi <herooopr@gmail.com>
2026-08-11 13:53:12 +0300

    nvim: fix flash plugin

--- modules/wrappers/neovim/lua/plugins/flash.lua
+++ modules/wrappers/neovim/lua/plugins/flash.lua
@@ -1,10 +1,11 @@
 return {
   "flash.nvim",
-
-  opts = {},
   keys = {
     { "<leader>z", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "<leader>Z", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-  },
 
+    { "f", mode = { "n", "x", "o" }, function() require("flash").jump({ mode = "char", search = { forward = true, wrap = false, multi_line = false } }) end },
+    { "F", mode = { "n", "x", "o" }, function() require("flash").jump({ mode = "char", search = { forward = false, wrap = false, multi_line = false } }) end },
+  },
+  opts = {},
 }
