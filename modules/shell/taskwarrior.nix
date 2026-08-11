9df83cd38268be626be25e1ecc6d0ad5ebd8ea35
Dallinchi <herooopr@gmail.com>
2026-08-11 13:46:44 +0300

    shell: taskwarrior implement add-active

--- modules/shell/taskwarrior.nix
+++ modules/shell/taskwarrior.nix
@@ -8,5 +8,34 @@
         color.active = "rgb045";
       };
     };
+
+    programs.fish = {
+      
+      # Create and start task
+      functions.task = ''
+        if test "$argv[1]" = "add-active"
+            if test (count $argv) -lt 2
+                echo "Additional text must be provided."
+                return 1
+            end
+            
+            set -l task_args $argv[2..-1]
+            
+            set -l task_id (command task add $task_args | string match -r 'Created task \K\d+')
+            if test -n "$task_id"
+                command task $task_id start
+            end
+
+        else
+            command task $argv
+        end
+      '';
+
+    };
+    
+    xdg.configFile."fish/completions/task.fish".text = ''
+      complete -c task -n "__fish_use_subcommand" -a add-active -d "operation:Create and immediately start a task"
+      '';
+
   };
 }
